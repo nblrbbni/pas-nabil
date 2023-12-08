203544e8778770bf4a7491c657f8bfe3924f6f2c
nblrbbni <muhammadrabbani463@smk.belajar.id>
2023-12-08 10:38:27 +0700

    pas-nabil

--- app/Http/Controllers/BeritaController.php
+++ app/Http/Controllers/BeritaController.php
@@ -4,31 +4,36 @@ namespace App\Http\Controllers;
 
 use App\Models\Berita;
 use App\Models\Kategori;
+use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\File;
+use File;
 
 class BeritaController extends Controller
 {
     public function index()
     {
-        $berita = Berita::get();
+        $berita = berita::get();
         return view('admin.berita.index', compact('berita'));
     }
 
     public function create()
     {
-        $kategori = Kategori::all();
-        return view('admin.berita.create', ['kategori' => $kategori]);
+        $kategori = Kategori::get();
+        $user     = User::get();
+        return view('admin.berita.create', compact('kategori', 'user'));
     }
 
     public function store(Request $request)
     {
+
         $request->validate([
+            'user_id'       => '',
             'kategori_id'   => 'required',
             'judul'         => 'required',
-            'gambar'        => 'image|mimes:jpg,png,jpeg',
+            'gambar'        => 'required|image|mimes:jpg,png,jpeg',
             'isi'           => 'required'
         ]);
+
 
         $gambarName = time() . '.' . $request->gambar->extension();
 
@@ -44,59 +49,69 @@ class BeritaController extends Controller
 
         $berita->save();
 
-        return redirect('/berita')->with('success', 'Data berhasil ditambahkan');
+        return redirect('/berita')->with('success', 'added data successfully');
     }
 
     public function edit($id)
     {
-        $kategori = Kategori::all();
+        $kategori = Kategori::get();
+        $user     = User::get();
         $berita   = Berita::findOrFail($id);
-        return view('admin.berita.edit', compact('berita', 'kategori'));
+        return view('admin.berita.edit', compact('berita', 'kategori', 'user'));
     }
 
     public function update(Request $request, $id)
     {
         $request->validate([
+            'user_id'       => '',
             'kategori_id'   => 'required',
             'judul'         => 'required',
             'gambar'        => 'image|mimes:jpg,png,jpeg',
             'isi'           => 'required'
         ]);
 
-        $berita = Berita::find($id);
 
         if ($request->has('gambar')) {
-            $this->deleteImage($berita->gambar);
+            $berita = Berita::find($id);
+
+            $path = "img/gambar/";
+            File::delete($path . $berita->gambar);
 
             $gambarName = time() . '.' . $request->gambar->extension();
+
             $request->gambar->move(public_path('img/gambar'), $gambarName);
-            $berita->gambar = $gambarName;
+
+            $berita->user_id     = auth()->user()->id;
+            $berita->kategori_id = $request->kategori_id;
+            $berita->judul       = $request->judul;
+            $berita->gambar      = $gambarName;
+            $berita->isi         = $request->isi;
+
+            $berita->save();
+
+            return redirect('/berita');
+        } else {
+            $berita = Berita::find($id);
+
+            $berita->user_id     = auth()->user()->id;
+            $berita->kategori_id = $request->kategori_id;
+            $berita->judul       = $request->judul;
+            $berita->isi         = $request->isi;
+
+            $berita->save();
+
+            return redirect('/berita');
         }
-
-        $berita->user_id     = auth()->user()->id;
-        $berita->kategori_id = $request->kategori_id;
-        $berita->judul       = $request->judul;
-        $berita->isi         = $request->isi;
-
-        $berita->save();
-
-        return redirect('/berita')->with('success', 'Update data berhasil');
     }
 
     public function destroy($id)
     {
         $berita = Berita::find($id);
-        $this->deleteImage($berita->gambar);
+
+        $path = "img/gambar/";
+        File::delete($path . $berita->gambar);
         $berita->delete();
 
-        return redirect('/berita')->with('success', 'Data berhasil dihapus');
-    }
-
-    protected function deleteImage($imageName)
-    {
-        $path = public_path('img/gambar/') . $imageName;
-        if (File::exists($path)) {
-            File::delete($path);
-        }
+        return redirect('/berita')->with('success', 'success, data deleted');
     }
 }
--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 
 class KategoriController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $kategori = Kategori::all();
 
         return view('admin.kategori.index', ['kategori' => $kategori]);
@@ -23,7 +24,7 @@ class KategoriController extends Controller
             'nama_kategori' => $request->nama_kategori,
         ]);
 
-        return redirect('/kategori')->with('success', 'Data berhasil ditambahkan');
+        return redirect('/kategori')->with('success', 'Kategori berhasil ditambahkan');
     }
 
     public function edit($id)
@@ -38,18 +39,17 @@ class KategoriController extends Controller
             'nama_kategori' => 'required',
         ]);
 
-        $kategori = Kategori::find($id);
+        Kategori::find($id)->update([
+            'nama_kategori' => $request->nama_kategori,
+        ]);
 
-        $kategori->nama_kategori = $request->nama_kategori;
-
-        $kategori->save();
-
-        return redirect('/kategori');
+        return redirect('/kategori')->with('success', 'Kategori berhasil diperbarui');
     }
 
     public function destroy($id)
     {
-        Kategori::where('id', $id)->delete();
-        return redirect()->to('/kategori')->with('success', 'The data has been successfully deleted');
+        Kategori::destroy($id);
+
+        return redirect()->to('/kategori')->with('success', 'Kategori berhasil dihapus');
     }
 }
